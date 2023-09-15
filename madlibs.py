fd44d28ee88594b13f10fefc8299d3d5d7d1145d
Niraj Sah <NP000771@mail.apu.edu.my>
2023-09-15 18:04:51 +0545

    Add new project

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# youtuber = "nirajofficals"
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscrube to {youtuber}")
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer Programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrted and {verb2} like you are {famous_person}!"
+print(madlib)
